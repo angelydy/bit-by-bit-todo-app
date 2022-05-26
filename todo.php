fe84bdccd7ef8605b0451fb99882614279aecccf
angely <angelymaedyy@gmail.com>
2022-05-26 14:33:45 +0800

    renames file folder

--- todo.php
+++ todo.php
@@ -3,11 +3,13 @@
 	<head>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
 		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
+      	<link rel="shortcut icon" href="/"/>
+      	<title>ToDo App | Finals</title>
 	</head>
 <body>
 	<div class="col-md-3"></div>
 	<div class="col-md-6 well">
-		<h3 class="text-primary">PHP - Simple To Do List App</h3>
+		<h3 class="text-primary">ToDo App HHEHEHE</h3>
 		<hr style="border-top:1px dotted #ccc;"/>
 		<div class="col-md-2"></div>
 		<div class="col-md-8">
